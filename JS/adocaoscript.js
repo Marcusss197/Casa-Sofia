3caae44bf5ee2504179bff03b54efd037c5f0179
Marcus <marcus2607@hotmail.com>
2024-04-17 21:46:54 -0300

    Fiz umas coisas + coisa da aula

--- JS/adocaoscript.js
+++ JS/adocaoscript.js
@@ -1,4 +1,106 @@
-let x=10
+/*
+let xOut=0;
+let yOut=0;
 
-console.log(x)
+var y=0;
+var x=1;
+
+function sum (a,b){
+    let x = a + b;
+    var y=x;
+    xOut= x;
+    yOut= x;
+
+}
+
+sum(1,1);
+
+console.log(y);
+console.log(xOut);
+console.log(yOut);
+
+for(var x=0;x<5;x++){
+    console.log(x);
+
+}
+console.log(x);
+
+
+var pudim="Leite condesado, leite e caramelo";
+var leite="vaca";
+
+function imprimi(){
+    var x;
+    x=450;
+    function juntar(){
+        var x1=x;
+        console.log(x1);
+    }
+
+    juntar()
+}
+
+imprimi();
+*/
+
+
+/*function nomeFuncao(parametro1,parametro2){
+    return parametro1+parametro2;
+}
+
+let a= nomeFuncao;
+
+console.log(a(1,2));*/
+
+/*var a=20;
+var b=30;
+
+const soma=(a,b) => a+b;
+const subtracao=(a,b) => a-b;
+const multi=(a,b) => a*b;
+const divisao=(a,b) => a/b;
+const mod=(a,b) => a%b;
+
+console.log(soma(a,b));
+console.log(subtracao(a,b));
+console.log(multi(a,b));
+console.log(divisao(a,b));
+console.log(mod(a,b));*/
+
+
+/*let pessoa=
+console.log(JSON.stringify(pessoa))
+JSON.stringify(pessoa) //vira json
+JSON.parse() //reverte
+
+console.log(pessoa.endereco.rua) //imprime somente o nome da rua
+console.log(pessoa.endereco) //mostra endereco, bairro, numero, etc
+
+*/
+
+let pessoa = {
+    nome: "Gustavo",
+    idade: 29,
+    endereco:{
+        rua:"Belarmino de mendonça",
+        numero: 9999,
+        bairro: "Centro",
+        cidade: "Foz do iguaçu"
+    },
+    formacaoAcademica:{
+        nomeFaculdade:"Faculdade x",
+        curso:{
+            nome:"Computacao",
+            disciplinas:[
+                "Computação 1",
+                "Computacao 2",
+                "Algoritmos",
+                "Compiladores",
+                "LFA"
+            ]
+        }
+    }
+}
+
+console.log(pessoa.formacaoAcademica.curso.disciplinas)
 
